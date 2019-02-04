4b488dd5f17fe32b282b524d92abc3ccde812d5e
Olga Ovcharova <olga.ovcharova@daxx.com>
2019-02-04 14:11:28 +0200

    services added

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -21,7 +21,7 @@ export class GameComponent implements OnInit {
     computerPlayerHand: []
   };
 
-
+  private readonly _WIN_SCORE: number = 21;
   private readonly _ENOUGH_SCORE: number = 15;
 
   private _deck: TCard[];
@@ -38,7 +38,7 @@ export class GameComponent implements OnInit {
     this.result.winner = '';
     this.result.humanScore = 0;
     this.result.computerScore = 0;
-    
+
     this._deck = this._deck.concat(this.playerHands.humanPlayerHand, this.playerHands.computerPlayerHand);
     this.playerHands.humanPlayerHand = [];
     this.playerHands.computerPlayerHand = [];
@@ -51,9 +51,9 @@ export class GameComponent implements OnInit {
   }
 
   public onHit(): void {
-    while (this.result.humanScore <= 21) {
-    this.playerHands.humanPlayerHand.push(this._deck.pop());
-    this.result.humanScore += this.playerHands.humanPlayerHand[this.playerHands.humanPlayerHand.length - 1].value;
+    while (this.result.humanScore <= this._WIN_SCORE) {
+      this.playerHands.humanPlayerHand.push(this._deck.pop());
+      this.result.humanScore += this.playerHands.humanPlayerHand[this.playerHands.humanPlayerHand.length - 1].value;
 
     return;  
     } 
@@ -61,12 +61,12 @@ export class GameComponent implements OnInit {
 
   public onStand(): void {
     this.result.isShownButtons = false;
-    
+
     while (this.result.computerScore <= this._ENOUGH_SCORE) {
       this.playerHands.computerPlayerHand.push(this._deck.pop());
       this.result.computerScore += this.playerHands.computerPlayerHand[this.playerHands.computerPlayerHand.length - 1].value;
     }
-    
+
     this.result.winner = this.winService.getWinner(this.result.humanScore, this.result.computerScore);
   }
 }
